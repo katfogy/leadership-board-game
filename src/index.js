ad5bf107a921818375dd0ea9969799df100af02b
katfogy <katfogy@gmail.com>
2023-05-18 00:18:30 +0100

    api call implemented

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './styles.css';
+import createScore from './modules/createScore.js';
+import LeaderBoard from './modules/leadersboard.js';
+
+const scores = new LeaderBoard();
+const btn = document.getElementById('add-score');
+scores.displayScores();
+const refreshBtn = document.getElementById('refresh');
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  createScore();
+});
+
+refreshBtn.addEventListener('click', () => {
+  document.location.reload();
+});
